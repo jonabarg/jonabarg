1e767ed73dea011efd865e6ac360fc951b864c28
jonabarg <97710262+jonabarg@users.noreply.github.com>
2025-05-13 18:52:07 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,2 @@
-// Store a reference to the <h1> in a variable
-const myHeading = document.querySelector("h1");
-// Update the text content of the <h1>
+const myHeading = document.querySelector("p");
 myHeading.textContent = "Hello world!";
